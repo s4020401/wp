6c4d84f3d5f145045ee36a4039221a9061242f88
zekun liu <132191306+s4020401@users.noreply.github.com>
2024-09-17 19:35:51 +1000

    Create index.php
    created a index.php file that is the index.html page.

---
+++ a2/index.php
@@ -0,0 +1,19 @@
+<?php
+include('includes/header.inc');
+include('includes/nav.inc');
+?>
+
+<main>
+    <div class="company">
+        <div class="text-container">
+            <h4>PETS VICTORIA</h4>
+            <h2>Welcome to Pet Adoption</h2>
+        </div>
+        <div class="company-image-container">
+            <img class="company-image" src="images/main.jpg" alt="Company Logo">
+        </div>
+    </div>
+</main>
+
+<?php
+?>
